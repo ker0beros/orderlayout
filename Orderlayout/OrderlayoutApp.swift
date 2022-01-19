5e1537068639671e0a6fe70997c76325274961e3
Khairul Azmi <azmi@getslurp.com>
2022-01-19 10:26:16 +0800

    Initial commit

---
+++ Orderlayout/OrderlayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderlayoutApp.swift
+//  Orderlayout
+//
+//  Created by Khairul Azmi on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct OrderlayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
